ed03230fe9c31556c935ab5b9c9dc031bebb7d64
WANNN-18 <118440426+FamilyOconner@users.noreply.github.com>
2023-03-09 05:02:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'RyuuOne'// yourname
+global.MONGO_DB_URI = "mongodb+srv://erwanoconner:<Fr9IhHXjiJBOiCyg>@cluster0.55u4nam.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
+global.your_email = "erwanoconner@gmail.com" //email
 global.email_password = "" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
