2c59d75e22957bd0e770844cb60fd7b5b64d6ca5
Renz16 <118329262+Renzstore0@users.noreply.github.com>
2023-04-01 13:29:35 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Renz'// yourname
+global.MONGO_DB_URI = "mongodb+srv://RenzKece:rasyid20100810@renz0.wup2umy.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "rasyidhamdi0@gmail.com" //email
+global.email_password = "cbnyrjedwjsdzcxp" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
