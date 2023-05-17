3d109d1a77e2f8de2a73ac945459fd9e0a4f49b8
Rian88768 <133885786+Rian88768@users.noreply.github.com>
2023-05-17 21:07:40 +0700

    Update settings.js
    Sippppp

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'samsul'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Samsul:9AYdNFXLVLtasvs7@cluster0.bsgejrq.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "pemburumakanan9@gmail.com" //email
+global.email_password = "tdsofbjshjxliewn" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
