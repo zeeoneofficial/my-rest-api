1ebe9644dcc75fe87b5b061603ef1d778fdf1e66
Deff sleebew <tatsuyakajima58@gmail.com>
2023-07-27 11:08:49 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'D-escobaR'// yourname
+global.MONGO_DB_URI = "L" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "deffstore72@gmail.com" //email
+global.email_password = "deffstore72@gmail.com" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
