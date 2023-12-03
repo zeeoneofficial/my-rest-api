cdd4332e83083ddb2b9b808f5819a932653832c2
Deff sleebew <septaraditya64@gmail.com>
2023-12-03 19:09:12 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = '乂Reisuke乂'// yourname
+global.MONGO_DB_URI = "mongodb+srv://smawurwow:yODmQSMHeAcmi4tr@cluster0.6128gap.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "orangbaik78665@gmail.com" //email
+global.email_password = "gwpn woit hbtc lnee" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
