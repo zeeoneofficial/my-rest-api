4af9aecf5e48695f0eec32f437bf46228a31e7d0
mario9998 <152520007+mario9998@users.noreply.github.com>
2023-12-15 12:00:47 +0700

    update setting.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://mario09:mario11081984@cluster0.gibvf4l.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "finky0822@gmail.com" //email
+global.email_password = "rpvi hqcz ssik kezz" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
